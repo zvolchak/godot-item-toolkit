58081a76a07abb370088f8ca2f1b6601c609ca1a
Zach <zach.bender@protonmail.com>
2025-07-02 16:14:05 -0600

    cleanup

--- addons/item_kit/Scripts/Editor/JsonData/JsonItemBaseData.cs
+++
@@ -1,8 +0,0 @@
-using Gamehound.ItemKit.Resources;
-
-namespace Gamehound.ItemKit.Editor;
-
-
-public class JsonItemBaseData {
-    public ItemResourceBase Item { get; set; }
-} // InventoryShapeData
--- addons/item_kit/Scripts/Interfaces/IResourceCreator.cs
+++ addons/item_kit/Scripts/Interfaces/IResourceCreator.cs
@@ -5,46 +5,6 @@ using Godot;
 namespace Gamehound.ItemKit.Interfaces;
 
 
-//public interface IResourceCreator<T> where T : Resource {
-
-//    public T CreateResource(
-//        string destination = null,
-//        ResourceOptions options = null
-//    );
-
-//    public string Hook_Preprocess(
-//        string path = null,
-//        ResourceOptions options = null
-//    );
-
-//    public T Hook_LoadResource(
-//        string path = null,
-//        ResourceOptions options = null
-//    );
-
-//    public T Hook_ProcessDuplicate(
-//        T existingResource,
-//        ResourceOptions options = null
-//    );
-
-//    public T Hook_SaveResource(
-//        T resource,
-//        string path = null,
-//        ResourceOptions options = null
-//    );
-
-
-//    public T Hook_Postprocess(
-//        T resource,
-//        string path = null,
-//        ResourceOptions options = null
-//    );
-
-//    public string GetFullPath();
-
-//} // IResourceCreator
-
-
 public interface IResourceCreator {
 
     public Resource CreateResource(
